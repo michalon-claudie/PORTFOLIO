2abc3678e876ee6622ed9ac68e4666cb2f13c7af
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-11-27 15:27:35 +0100

    "second commit"

---
+++ Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../component/Header'
+
+export default function About()
+{
+    return (
+        <div>
+            <Header/>
+            <h1></h1>
+        </div>
+    )
+}
