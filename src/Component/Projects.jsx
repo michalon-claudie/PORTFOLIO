8152231dae2532ec540ed3e9735561630e64d995
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-02-16 11:31:58 +0100

    changes styles and add porject container

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import projectsData from '../Projects.json'
 
 export default function Projects(){
     return(
         <section className='Projects'>
             <h2>Mes projets</h2>
             <div className='projectsContainer'>
+                {projectsData.map(project => (
+                    <div key={project.id} className='project'>
+                        <img src={project.img} alt={project.title} />
+                        <h3>{project.title}</h3>
+                        <p>Date: {project.date}</p>
+                        <p>Languages: {project.languages.join(', ')}</p>
+                        <p>{project.infos || project.description}</p>
+                        {project.link && <a href={project.link}>Voir le projet</a>}
+                    </div>
+                ))}
             </div>
         </section>
     )
