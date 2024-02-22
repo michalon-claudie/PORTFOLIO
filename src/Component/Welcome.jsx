113cbf8543c5496be7bcfaa6793a6f3bf421767b
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-02-22 12:10:59 +0100

    add aria label for accesibility

--- src/Component/Welcome.jsx
+++ src/Component/Welcome.jsx
@@ -42,13 +42,13 @@ return (
         <div className= 'helloAndContactContainer'>
             <p>{message}</p>
             <div className='iconContact'>
-                <a href="https://github.com/michalon-claudie">
+                <a href="https://github.com/michalon-claudie" aria-label='Link to Github'>
                     <FontAwesomeIcon icon={faGithub} />
                 </a>
-                <a href="https://www.linkedin.com/in/claudie-michalon-8b1a0422a/">
+                <a href="https://www.linkedin.com/in/claudie-michalon-8b1a0422a/"aria-label='Link to LinkedIn'>
                     <FontAwesomeIcon icon={faLinkedinIn} />
                 </a>
-                <a href="MAILTO:michalon.claudie@outlook.fr">
+                <a href="MAILTO:michalon.claudie@outlook.fr" aria-label='Send me an email'>
                     <FontAwesomeIcon icon={faEnvelope} />
                 </a>
                 <FontAwesomeIcon icon={faPhoneVolume} onClick={handlePhoneIconClick} className={showPhoneNumber ? 'active' : ''} />
