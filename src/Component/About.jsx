b37722a5000a59e12e5e4798edac0a88080e004e
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-03-14 15:11:35 +0100

    chanes about and add media querries on nav

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -12,13 +12,10 @@ export default function About(){
                 Suite à de multiples recherches et beaucoup d'apprentissage en autodidacte, 
                 j'ai décidé de m'orienter dans le développement web, en particulier dans le domaine du Frontend.
                 <br/>
-                J'ai suivi une formation intensive sur la plateforme OpenClassrooms, où j'ai acquis de 
-                nombreuses compétences mais aussi fait de superbes rencontres.<br/>
-                Toutefois, je continue mon apprentissage chaque jour, et je me suis lancée le defi de me former au Back-end. 
+                Chaque jour, je continues mon apprentissage et je me suis lancée le defi de me former au Back-end. 
                 Je suis animée par la volonté de créer des expériences numériques convaincantes et accessibles,
                 tout en veillant à ce qu'elles respectent les normes éthiques et environnementales. 
-                Mon parcours atypique m'a apporté une perspective unique, alliant sensibilité humaine et expertise technique,
-                que je mets au service de chaque projet que j'entreprends.<br/>
+                Mon parcours atypique m'a apporté une perspective unique, alliant sensibilité humaine et expertise technique.<br/>
             </p>
         </section>
     )
