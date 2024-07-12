e9370c923fc0afc2124be04c7a20786dc8f226a1
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-07-12 10:32:20 +0200

    adjust paragraphe about

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -12,7 +12,7 @@ export default function About(){
                 Suite à de multiples recherches et beaucoup d'apprentissage en autodidacte, 
                 j'ai décidé de m'orienter dans le développement web, en particulier dans le domaine du Frontend.
                 <br/>
-                Chaque jour, je continues mon apprentissage et je me suis lancée le defi de me former au Back-end. 
+                Chaque jour, je continue mon apprentissage et je me suis lancée le defi de me former au Back-end. 
                 Je suis animée par la volonté de créer des expériences numériques convaincantes et accessibles,
                 tout en veillant à ce qu'elles respectent les normes éthiques et environnementales. 
                 Mon parcours atypique m'a apporté une perspective unique, alliant sensibilité humaine et expertise technique.<br/>
