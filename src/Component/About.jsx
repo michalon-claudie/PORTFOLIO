0a62f4ad52aefe404046ae16a626af47ca4d9d6b
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-08-27 15:59:22 +0200

    paragraphe changed

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -13,7 +13,12 @@ export default function About(){
                 j'ai décidé de m'orienter dans le développement web, en particulier dans le domaine du Frontend.
                 <br/>
                 Une fois diplomée, j'ai mis en pratique mes connaissances. Une dynamique start-up Troyenne m'a fait confiance et m'a permit de mettre en pratique ce que j'avais appris. 
-                De plus, durant l'exercice de mes fonctions,  j'ai commencé à me former au back-end et au gestion de bases de données.
+                De plus, durant l'exercice de mes fonctions,  j'ai pu me former au back-end, aux concepts d'UX/UI design et au gestion de bases de données.
+                J'aime apprendre seule mais apprendre auprès de professionels passionés fut très enrichissant. 
+                Toutefois, cette expérience fut écourtée en raisons de problèmes économiques au sein de l'entreprise. 
+                C'est pourquoi je suis à la recherche d'une nouvelle opportunité.
+                <br/>
+                Mon interet ne fait que grandir au fil de mes projets.
                 Chaque jour, je continues de créer des projets et de m'exercer afin de proposer des expériences numériques dynamiques et intéréssantes.<br/>
                 N'hésitez pas à parcourir mon site et à bientot !
             </p>
