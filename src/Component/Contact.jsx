2b7f8b6cc0784925238617a2f237b06b4dccfe22
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-12-05 12:59:17 +0100

    add more icon on contact section

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -3,6 +3,8 @@ import '../Style/Contact.scss'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 export default function Contact(){
 
@@ -10,6 +12,12 @@ export default function Contact(){
         <section id="Contact"className='ContactMe'>
             <h1>Un projet en tête ? Parlons-en !</h1>
             <div className='iconContact'>
+            <a href="https://github.com/michalon-claudie" aria-label='Link to Github'>
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a href="https://www.linkedin.com/in/claudie-michalon/"aria-label='Link to LinkedIn'>
+                    <FontAwesomeIcon icon={faLinkedinIn} />
+                </a>
                 <a href ="MAILTO:michalon.claudie@outlook.fr" aria-label='Send email'>
                     <FontAwesomeIcon icon={faEnvelope} />
                 </a>
